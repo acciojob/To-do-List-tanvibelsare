86654ed4b90393365be51e3b995082f7c04d716e
tanvibelsare <118069064+tanvibelsare@users.noreply.github.com>
2023-02-19 12:54:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 //your code here
+var todoValue = document.getElementById("newTodoInput");
+var btn = document.getElementById("addTodoBtn");
+var ol = document.getElementById("todoList");
 
+function addTodoList() {
+  //alert(todoValue.value);
+  if (todoValue.value) {
+    // we need to create a li
+    var li = document.createElement("li");
+    li.textContent = todoValue .value;
+    ol.appendChild(li);
+    todoValue .value = "";
+  }
+}
+
+btn.addEventListener("click", addTodoList);
+  
